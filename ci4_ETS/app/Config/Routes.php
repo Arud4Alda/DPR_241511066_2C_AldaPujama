e04a0f33eb9f32d8b33d41b836611ab43c6487bb
aruda <aldpj27@gmail.com>
2025-10-02 16:31:52 +0700

    Revert "Add Fitur Lihat Komponen Gaji by Alda Pujama"
    This reverts commit ac3a49e82752e624ae5e992cbdbf2e320d8a4027.

--- ci4_ETS/app/Config/Routes.php
+++ ci4_ETS/app/Config/Routes.php
@@ -6,16 +6,3 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/login', 'Login::index');
-$routes->post('/login/auth', 'Login::auth');
-$routes->get('/logout', 'Login::logout');
-
-// Admin routes
-$routes->group('admin', ['filter' => 'adminauth'], function($routes) {
-    $routes->get('dashboard', 'Admin::dashboard');
-});
-
-// Student routes
-$routes->group('user', ['filter' => 'userauth'], function($routes) {
-    $routes->get('dashboard', 'user::dashboard');
-});
